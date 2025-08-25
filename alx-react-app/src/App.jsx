171a8929e26a37e912e59a978d24ccdf9aacf6fc
Emmanuel Wanyama <160263746+wanyamae@users.noreply.github.com>
2025-08-25 22:08:50 +0300

    Completed assignment 3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
     <Header />
     <MainContent />
     <WelcomeMessage />
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       
       <div>
         
