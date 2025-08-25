755928f57e01fe6b7ea9b64827bfed4907f0a8c5
Emmanuel Wanyama <160263746+wanyamae@users.noreply.github.com>
2025-08-25 21:51:01 +0300

    Completed task 1

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage () {
+    return (
+        <div>
+            <h1>Hello Everyone, I am Learning React at ALX!</h1>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage
