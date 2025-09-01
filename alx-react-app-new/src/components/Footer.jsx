c2de834d231e6b8f8f8e32a942c0e5787c5cb3d6
Emmanuel Wanyama <160263746+wanyamae@users.noreply.github.com>
2025-09-01 21:27:54 +0300

    Add styling to Footer component for improved appearance

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer () {
     return (
-        <footer>
+        <footer style={{ backgroundColor: '#f1f1f1', textAlign: 'center', padding: '10px', marginTop: '20px'    }}>
             <p>© 2023 City Lovers</p>
         </footer>
     );
