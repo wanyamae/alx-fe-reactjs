b06c3b9975b8e064749003ea2564ffc90339552c
Emmanuel Wanyama <160263746+wanyamae@users.noreply.github.com>
2025-09-01 21:56:09 +0300

    Refactor UserProfile component to use context for user data management

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,20 @@
-const UserProfile = (props) => {
-   return (
-     <div>
-       <h2>{props.name}</h2>
-       <p>Age: {props.age}</p>
-       <p>Bio: {props.bio}</p>
-     </div>
-   );
- };
+import React from 'react';
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
 
- export default UserProfile;
+function UserProfile(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+}
+
+function UserProfileWithContext(props) {
+  const context = useContext(UserContext);
+  return <UserProfile {...context} {...props} />;
+}
+
+export default UserProfileWithContext;
