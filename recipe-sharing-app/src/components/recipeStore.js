cb2662fdff9abfcc9f45cae03db6e8f353c22c3d
wanyamae <w.nakhanya@gmail.com>
2025-09-13 17:10:40 +0300

    fix: correct typo in useRecipeStore definition

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,6 +1,6 @@
 import create from 'zustand'
 
-const useRecipStore = create(set => ({
+const useRecipeStore = create(set => ({
     recipes: [],
     addRecipe: (newRecipe) => set(state => ({ recipes:
         [...state.recipes, newRecipe]
