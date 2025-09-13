d867816488079dbb12f898bb25bbd5985c4ddbd5
wanyamae <w.nakhanya@gmail.com>
2025-09-13 17:28:37 +0300

    fix: correct import statement and export usage in recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,6 +1,6 @@
-import create from 'zustand'
+import { create } from 'zustand'
 
-const useRecipeStore = create(set => ({
+export const useRecipeStore = create(set => ({
     recipes: [],
     addRecipe: (newRecipe) => set(state => ({ recipes:
         [...state.recipes, newRecipe]
@@ -8,10 +8,9 @@ const useRecipeStore = create(set => ({
     setRecipes: (recipes) => set({ recipes }),
 
     deleteRecipe: (id) => set(state => ({ recipes: state.recipes.filter(recipe => recipe.id !== id) })),
-    
+
     updateRecipe: (updatedRecipe) => set(state => ({ recipes: state.recipes.map(recipe => 
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
     ) })),
-}));
-
-export default useRecipeStore;
+})
+)
